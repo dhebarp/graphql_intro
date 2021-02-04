d66683d80ad094320e6a1d58be1af47a18f35151
paritosh <paritosh.dhebar@gmail.com>
2021-02-04 19:30:08 +1100

    added react and express projects and set up boilerplate code

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const port = 5000
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express()
+
+
+app.use('/graphql', graphqlHTTP({
+
+}))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
